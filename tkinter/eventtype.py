01a8ce4df76c0595c0a156bdfcc3eea3802cfe6a
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-03-05 09:46:30 +0800

    new

--- tkinter/eventtype.py
+++ tkinter/eventtype.py
@@ -11,12 +11,40 @@ root.title('事件绑定')
 
 root.minsize(300,300)
 
+# 鼠标点击操作
+'''
 btn1 = Button(root,text='按钮1')
 
 def one(evt) :
-    print('123')
+    print('左键')
 
+def two(evt) :
+    print('右键')
+
+def thr(evt) :
+    print('中键')
+
+# 鼠标左键
 btn1.bind('<Button-1>',one)
+# 鼠标右键
+btn1.bind('<Button-2>',two)
+# 鼠标中键
+btn1.bind('<Button-3>',thr)
+'''
+
+# 鼠标进入操作
+btn1 = Button(root,text = '按钮1')
+
+def mouse_in(evt) :
+    print('鼠标进入')
+
+def mouse_out(evt) :
+    print('鼠标离开')
+
+# 鼠标进入
+btn1.bind('<Enter>',mouse_in)
+# 鼠标离开
+btn1.bind('<Leave>',mouse_out)
 
 btn1.grid(row=0,column=0)
 
